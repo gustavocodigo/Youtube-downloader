c7fa8c674d345c1fc929d80152948454b932bbe2
Glitch (glitch-hello-website) <none>
2023-04-09 02:43:23 +0000

    👼🎺 Checkpoint ./script.js:47946017/1060

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+function isValidYoutubeVideoLink(link) {
+  // Expressão regular para validar um link de vídeo do YouTube
+  var regex = /^(https?\:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9\-_]{11})$/
+
+  // Verifica se o link corresponde ao padrão da expressão regular
+  return regex.test(link)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 let locked = false
 
 function search_video(url)
 {
   if (locked) return
+  if ( isValidYoutubeVideoLink(url)) {
   locked = true
   document.getElementById("loading-search").style.display = "block"
   
@@ -11,4 +31,10 @@ function search_video(url)
     document.getElementById("loading-search").style.display = "none"
     locked = false
   }, 3000)
+  }else{
+    alert("Error invalid youtbe link")
+  }
 }
+
+
+function convertClick() 
